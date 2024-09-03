288bb1aeade61a61c514947200da3887ee5517de
Erin DeVine <erinj291@gmail.com>
2024-09-03 19:35:03 -0400

    pushing cahnges to sw and webpack as well as js files

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,10 +2,31 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    console.log("before install");
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+
+});
+
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    console.log('Install button is clicked');
+    const promptEvent = window.deferredPrompt;
+    if (!promptEvent) {
+        console.log('Cancelling install...')
+        return;
+    }
+
+    promptEvent.prompt();
+
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('Installation complete')
+    window.deferredPrompt = null; 
+});
